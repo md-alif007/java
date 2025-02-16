bbaa52e09e75d60df0ad7194d4df342360547c3f
md-alif007 <mohammadalif043@gmail.com>
2025-02-16 12:20:31 +0600

    Taking input of array and finding the index of user input

---
+++ arrayIndex.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class arrayIndex {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size of your array : ");
+            int size = sc.nextInt();
+
+            int[] arr = new int[size];
+
+            System.out.println("Enter " + size + " elements : ");
+
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Your entered array is : ");
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+
+            System.out.println("\nWhich number do you want to find: ");
+            int query = sc.nextInt();
+
+            boolean found = false;
+            for (int i = 0; i < size; i++) {
+                if (query == arr[i]) {
+                    System.out.println("Number " + query + " is found at index " + i);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.out.println("Invalid Input");
+            }
+        }
+
+    }
+}
